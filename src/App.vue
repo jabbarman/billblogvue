65dbb3238928bd2d36e131631c7497acc433b00e
Joseph Jabbar <jabbarman@gmail.com>
2019-01-09 10:06:54 +0000

    created basic homepage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <nav>
+        <ul>
+            <li class="nav-item"><a href="#">View all posts</a></li>
+            <li class="nav-item"><a href="#">Add a post</a></li>
+        </ul>
+      </nav>
+    </header>
+    <main>
+      <HomePage />
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  color: #444;
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "HelveticaNeue-Light", "Helvetica Neue Light", "Helvetica Neue", Helvetica, Arial, "Lucida Grande", sans-serif;
+  font-weight: 400;
+  height: 100%;
+  min-height: 100%;
+  text-rendering: optimizeLegibility;
+}
+header {
+  background-color: rgb(140, 193, 193);
+  border-bottom: 1px solid rgba(0,0,0,.15);
+  display: flex;
+  flex-direction: column;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+ul {
+  list-style-type: none;
+  padding-left: 0;
+  display: flex;
+  flex-direction: row;
+  margin-bottom: 10px;
+  justify-content: space-between;
+  align-items: flex-end;
+  flex-wrap: wrap;
+}
+.nav-item {
+  box-sizing: border-box;
+  flex: 1;
+  padding: 0 10px;
+  text-align: center;
 }
 </style>
